74da82a3d442b67a22ccd8a848271c9e0c0e76dd
이예진 <bella_floret@naver.com>
2024-12-26 11:39:08 +0900

    add get

--- src/main/java/sessac/dev/sell/domain/item/ProductController.java
+++ src/main/java/sessac/dev/sell/domain/item/ProductController.java
@@ -90,6 +90,13 @@ public class ProductController {
         return showMessageAndRedirect(message,model);
     }
 
+    @GetMapping("/")
+    public String openProductListFromRott(Model model) {
+        // 상품 리스트를 모델에 추가
+        model.addAttribute("items",itemService.findAllItem());
+        return "item/product"; // templates/product.html을 반환
+    }
+
     // 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
     private String showMessageAndRedirect(final MessageDto params, Model model) {
         model.addAttribute("params", params);
