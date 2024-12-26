6494e9b2c34c6d41ebc1b738604fd1ee66a500b3
이예진 <bella_floret@naver.com>
2024-12-26 10:40:38 +0900

    cors error

--- src/main/java/sessac/dev/sell/config/WebMvcConfig.java
+++ src/main/java/sessac/dev/sell/config/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package sessac.dev.sell.config;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import sessac.dev.sell.interceptor.LoggerInterceptor;
 import sessac.dev.sell.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -17,5 +19,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .addPathPatterns("/**/*.do")
                 .excludePathPatterns("/log*");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://34.22.109.79:8081") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 자격 증명 허용
     }
 }
